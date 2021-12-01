a27b29eea4605c256a67e0d60ccdadb1a2a3ca5e
Edb83 <ed3radley@gmail.com>
2021-12-01 14:48:37 +0000

    Add Sidebar component and hook up to css

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+const Sidebar = () => {
+    return (
+    <div className = {css.sidebar} >
+        <a href="#" target="_blank">MyPhotos</a>
+        <a href="#" target="_blank">MyIllustrations</a>
+        <a href="#" target="_blank">MyPaintings</a>
+    </div>
+    )
+}
+
+export default Sidebar;
